f487cb779031f0df19f063d6880685c05db0ab6d
Felipe Vasconcelos <felipe.vasconcelos@semantix.ai>
2024-09-12 12:12:50 -0300

    feat: Thresholds

---
+++ modulo2/7-thresholds.js
@@ -0,0 +1,26 @@
+// 1. Inicialização
+import http from 'k6/http';
+import { check } from 'k6';
+import { Counter, Gauge, Rate, Trend } from 'k6/metrics';
+
+// 2. Configuração
+export const options = {
+  vus: 1,
+  duration: '3s',
+  thresholds: {
+    http_req_failed: ['rate < 0.01'],
+    http_req_duration: [{threshold: 'p(95) < 200', abortOnFail: true, delayAbortEval: '10s' }],
+    checks: ['rate > 0.99']
+  }
+}
+// 3. Execução
+export default function() {
+  const res = http.get('http://test.k6.io');
+  
+  check(res, {
+    'status code is 200': (r) => r.status === 200
+  })
+}
+
+
+// running with "k6 run modulo2/7-thresholds.js" 
